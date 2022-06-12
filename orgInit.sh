6b45229cdf5073ba79b1fa6f974ba9fe912f22ac
Radif Masud <rmasud@salesforce.com>
2022-06-12 07:53:57 -0500

    added puppateer script to set the metadata data settings

--- orgInit.sh
+++ orgInit.sh
@@ -3,8 +3,8 @@
 #create scratch org
 sfdx force:org:create -s -f config/project-scratch-def.json -d 14 -s -w 60
 
-#install OmniStudio
-sfdx force:package:install -p 04t4W000002kdlN -w 45 -r 
+#install OmniStudio - 238
+sfdx force:package:install -p 04t4W000002ke26QAA -w 45 -r 
 
 #push the utility metadata service class
 sfdx force:source:deploy -m ApexClass:MetadataService
@@ -12,7 +12,8 @@ sfdx force:source:deploy -m ApexClass:MetadataService
 #set the remote sites
 sfdx force:apex:execute -f dparemoteSites.cls
 
-#TODO - Puppateer to set OmniStudio Metadata to TRUE via UI 
+#Puppateer to set OmniStudio Metadata to TRUE via UI 
+node run.js
 
 #TODO- Push metadata example into the org (inactive)
 
---
+++ run.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+
+const puppeteer = require('puppeteer');
+const utils = require('./utils');
+
+(async function() {
+
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-features=site-per-process',
+
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
+    });
+
+
+    console.log(process.argv);
+    var targetUsername = process.argv[2]; //0 index
+
+    var frontdoor = null;
+    if (targetUsername)
+        frontdoor = await utils.runSFDXCommand(`sfdx force:org:open -u ${targetUsername} -r --json`);
+    else
+        frontdoor = await utils.runSFDXCommand(`sfdx force:org:open -r --json`);
+
+    console.log(frontdoor.result.url);
+
+    var urlData = frontdoor.result.url;
+    var scubbedUrl = urlData.replace("https://", "");
+    var parsedUrl = scubbedUrl.split('/');
+    var fqdn = parsedUrl[0];
+    var parsedFqdn = fqdn.split('.');
+
+
+    const timeout = 30000;
+    const page = await browser.newPage();
+    page.setDefaultTimeout(60000);
+    await page.goto(`${frontdoor.result.url}`);
+
+    //await page.waitFor(10000);
+    //go to /lightning/setup/OmniStudioSettings/home
+    var targetOSSettings = `https://${parsedFqdn[0]}.lightning.force.com/lightning/setup/OmniStudioSettings/home`;
+    console.log(targetOSSettings)
+
+    await Promise.all([
+        page.waitForNavigation({ timeout: timeout, waitUntil: 'load' }),
+        page.waitForNavigation({ timeout: timeout, waitUntil: 'networkidle2' }),
+        page.goto(targetOSSettings)
+    ]);
+
+    await page.setViewport({ width: 1200, height: 837 });
+    await utils.sleep(10000);
+
+
+    //OmniStudio Runtime
+    try {
+        await page.evaluateHandle(
+            () => document.querySelectorAll('runtime_omnistudio-pref-toggle')[1].shadowRoot.querySelector('lightning-input').shadowRoot.querySelector('input').click()
+        );
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    //Standard OmniStudio
+    try {
+        await page.evaluateHandle(
+            () => document.querySelectorAll('runtime_omnistudio-pref-toggle')[1].shadowRoot.querySelector('lightning-input').shadowRoot.querySelector('input').click()
+        );
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    await browser.close();
+})();
