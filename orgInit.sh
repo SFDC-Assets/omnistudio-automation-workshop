a4b84e0082b928f7b2b80e244088bafc03353bd2
Radif Masud <rmasud@salesforce.com>
2022-06-12 08:18:52 -0500

    added sample metdata to use OmniStudio metadata

--- orgInit.sh
+++ orgInit.sh
@@ -4,7 +4,7 @@
 sfdx force:org:create -s -f config/project-scratch-def.json -d 14 -s -w 60
 
 #install OmniStudio - 238
-#sfdx force:package:install -p 04t4W000002ke26QAA -w 45 -r 
+sfdx force:package:install -p 04t4W000002ke26QAA -w 45 -r 
 
 #push the utility metadata service class
 sfdx force:source:deploy -m ApexClass:MetadataService
@@ -12,10 +12,11 @@ sfdx force:source:deploy -m ApexClass:MetadataService
 #set the remote sites
 sfdx force:apex:execute -f dparemoteSites.cls
 
-#Puppateer to set OmniStudio Metadata to TRUE via UI 
+#Puppateer to set OmniStudio Metadata to TRUE via UI  and enable the  standard runtime
 node run.js
 
-#TODO- Push metadata example into the org (inactive)
+#Sample metadata examples into the org - basic hellow world.
+sfdx force:source:deploy -m OmniUiCard,OmniProcess,OmniIntegrationProcedure,OmniDataTransform
 
 #open says me.
 sfdx force:org:open
