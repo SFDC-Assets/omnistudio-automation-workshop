b4732a624af76edb737fc52528e0a7a7c3836f26
zhou059 <zhou059@gmail.com>
2022-08-24 10:45:19 -0700

    others

---
+++ force-app/main/default/lwc/cfAccountAndExternalData/cfAccountAndExternalData.js
@@ -0,0 +1,56 @@
+import { FlexCardMixin } from "omnistudio/flexCardMixin";
+    import {interpolateWithRegex, interpolateKeyValue, loadCssFromStaticResource } from "omnistudio/flexCardUtility";
+    
+          import { LightningElement, api, track, wire } from "lwc";
+          import pubsub from "omnistudio/pubsub";
+          import { getRecord } from "lightning/uiRecordApi";
+          
+          import data from "./definition";
+          
+          import styleDef from "./styleDefinition";
+              
+          export default class cfAccountAndExternalData extends FlexCardMixin(LightningElement){
+              @api debug;
+              @api recordId;
+              @api objectApiName;
+              
+              @track record;
+              
+              
+              pubsubEvent = [];
+              customEvent = [];
+              
+              connectedCallback() {
+                super.connectedCallback();
+                this.setStyleDefinition(styleDef);
+                data.Session = {} //reinitialize on reload
+                
+                
+                
+                this.setDefinition(data);
+ this.registerEvents();
+                
+                
+              }
+              
+              disconnectedCallback(){
+                super.disconnectedCallback();
+                    
+                    
+
+                  this.unregisterEvents();
+              }
+
+              registerEvents() {
+                
+              }
+
+              unregisterEvents(){
+                
+              }
+            
+              renderedCallback() {
+                super.renderedCallback();
+                
+              }
+          }
