eeea2737e651110796abad69591db30052e9be5b
zhou059 <zhou059@gmail.com>
2022-09-09 15:45:51 -0700

    checkin

--- orgInit.sh
+++ orgInit.sh
@@ -4,14 +4,16 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix omni -o studio.workshop
 
 #install FSC
-sfdx force:package:install --package=04t1E000000jb9R --wait 15
-sfdx force:package:install --package=04t1E000001Iql5 --wait 2
+# sfdx force:package:install --package=04t1E000000jb9R --wait 15
+# sfdx force:package:install --package=04t1E000001Iql5 --wait 2
 
 #install omnistudio 238.3
 sfdx force:package:install --package=04t4W000002kepJ --wait 15
 sfdx shane:user:password:set -p salesforce1 -g User -l User
-sfdx force:user:permset:assign -n FinancialServicesCloudStandard
-sfdx force:user:permset:assign -n FinancialServicesCloudExtension
+# sfdx force:user:permset:assign -n FinancialServicesCloudStandard
+# sfdx force:user:permset:assign -n FinancialServicesCloudExtension
+
+sfdx force:community:create --name 'Product Interest' --templatename 'Customer Service' --urlpathprefix ProductInterestHome
 
 #push the utility metadata service class
 sfdx force:source:deploy -m ApexClass:MetadataService
