160f88c0ceaee995e43a993c0da87f60f08796af
mehboob <77236545+mehb1@users.noreply.github.com>
2025-04-07 08:44:00 +0000

    change

--- orgInit.sh
+++ orgInit.sh
@@ -3,5 +3,4 @@
 # https://hosted-scratch.herokuapp.com/launch?template=https://github.com/SFDC-Assets/omnistudio-automation-workshop
 sf force:org:open -o deploybot@dxhub.org -r
 #create scratch org
-#example
 sf shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix omni -o studio.workshop
